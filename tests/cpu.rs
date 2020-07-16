7238817c866584e28f004ac074ccea00f38aada8
chenpenghui <chenpenghui@sensetime.com>
2020-07-16 21:02:38 +0800

    feat(cpu): add cycles and impl several instruction

--- tests/cpu.rs
+++ tests/cpu.rs
@@ -8,9 +8,9 @@ fn mapper0() {
     let mapper0 = Mapper0::new(&mut cartridge);
     let cpu = CPU::new(mapper0);
 
-    assert_eq!(0xc5af, cpu.nmi().unwrap());
-    assert_eq!(0xc004, cpu.rst().unwrap());
-    assert_eq!(0xc5f4, cpu.irq().unwrap());
+    assert_eq!(0xc5af, cpu.nmi());
+    assert_eq!(0xc004, cpu.rst());
+    assert_eq!(0xc5f4, cpu.irq());
 }
 
 #[test]
@@ -20,14 +20,12 @@ fn run() {
     let mapper0 = Mapper0::new(&mut cartridge);
     let mut cpu = CPU::new(mapper0);
 
-    cpu.jmp_c000();
+    cpu.set_PC(0xc000);
+    cpu.set_cycles(7);
     println!("{}", cpu.debug_info());
-    cpu.run();
-    println!("{}", cpu.debug_info());
-    cpu.run();
-    println!("{}", cpu.debug_info());
-    cpu.run();
-    println!("{}", cpu.debug_info());
-    cpu.run();
-    println!("{}", cpu.debug_info());
+
+    for n in 1..10 {
+        cpu.run();
+        println!("{}", cpu.debug_info());
+    }
 }
