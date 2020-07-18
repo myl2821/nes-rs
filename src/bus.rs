333b615f372214b327bfdb49e500ff6866306e4b
Mo Yuli <moyuli@sensetime.com>
2020-07-19 00:58:46 +0800

    chore: move memory map comment to bus

--- src/bus.rs
+++ src/bus.rs
@@ -3,6 +3,18 @@ use crate::{Mapper, CPU};
 use std::cell::RefCell;
 use std::rc::Rc;
 
+// Bus Memory map
+// -------------------------------------------------------------------------------------------------
+// Address range |  Size   |  Device
+// $0000-$07FF	 |  $0800  |  2KB internal RAM
+// $0800-$0FFF	 |  $0800  |  Mirrors of $0000-$07FF
+// $1000-$17FF	 |  $0800  |  Mirrors of $0000-$07FF
+// $1800-$1FFF	 |  $0800  |  Mirrors of $0000-$07FF
+// $2000-$2007	 |  $0008  |  NES PPU registers
+// $2008-$3FFF	 |  $1FF8  |  Mirrors of $2000-2007 (repeats every 8 bytes)
+// $4000-$4017	 |  $0018  |  NES APU and I/O registers
+// $4018-$401F	 |  $0008  |  APU and I/O functionality that is normally disabled. See CPU Test Mode
+// $4020-$FFFF	 |  $BFE0  |  Cartridge space: PRG ROM, PRG RAM, and mapper registers (See Note)
 pub struct Bus<T: Mapper> {
     ram: [u8; 0x0800],
     mapper: T,
--- src/cpu.rs
+++ src/cpu.rs
@@ -123,18 +123,6 @@ struct Info {
     mode: Mode,
 }
 
-// CPU Memory map
-// -------------------------------------------------------------------------------------------------
-// Address range |  Size   |  Device
-// $0000-$07FF	 |  $0800  |  2KB internal RAM
-// $0800-$0FFF	 |  $0800  |  Mirrors of $0000-$07FF
-// $1000-$17FF	 |  $0800  |  Mirrors of $0000-$07FF
-// $1800-$1FFF	 |  $0800  |  Mirrors of $0000-$07FF
-// $2000-$2007	 |  $0008  |  NES PPU registers
-// $2008-$3FFF	 |  $1FF8  |  Mirrors of $2000-2007 (repeats every 8 bytes)
-// $4000-$4017	 |  $0018  |  NES APU and I/O registers
-// $4018-$401F	 |  $0008  |  APU and I/O functionality that is normally disabled. See CPU Test Mode
-// $4020-$FFFF	 |  $BFE0  |  Cartridge space: PRG ROM, PRG RAM, and mapper registers (See Note)
 impl<T: Mapper> CPU<T> {
     pub fn new() -> Self {
         let mut cpu = CPU {
